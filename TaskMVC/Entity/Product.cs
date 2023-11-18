a37bc534e7895e7288012fe5add2e9f97cf78b11
xusanchik <samadovxusan9013@gmail.com>
2023-11-18 15:50:20 +0500

    news

--- TaskMVC/Entity/Product.cs
+++ TaskMVC/Entity/Product.cs
@@ -1,10 +1,17 @@
-﻿namespace TaskMVC.Entity
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskMVC.Entity
 {
     public class Product
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "This product is available")]
         public string Title { get; set; }
+        [Range(0, double.MaxValue, ErrorMessage = "The Amout cannot be negative")]
         public double Quantiy { get; set; }
+        //[Required(ErrorMessage = "Narxi manfiy bo'lishi mumkin emas")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "The Price cannot be negative")]
         public double Price { get; set; }
         public double TotolPrice { get; set; }
     }
